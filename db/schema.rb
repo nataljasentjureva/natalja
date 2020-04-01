0080832983aa831c3a39506feb4e392fd28708e6
Natalja Sentjureva <natalja@benuevo.com>
2020-04-01 14:21:55 +0300

    #1 add email confirmation for admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_100719) do
+ActiveRecord::Schema.define(version: 2020_04_01_105854) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2020_04_01_100719) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
